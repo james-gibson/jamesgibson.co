07ab8a3f4dba5b95bdae4f3efdb921e325f149bd
James Gibson <james.the.nodester@gmail.com>
2024-08-21 17:28:25 -0600

    new contact info

--- src/views/Contact/index.tsx
+++ src/views/Contact/index.tsx
@@ -3,16 +3,21 @@ import "./styles.scss";
 function Contact() {
   return (
     <div className="contact">
-      <h1>Contact me</h1>
+      <h1>Find me</h1>
 
       <a href="https://www.linkedin.com/in/jameswgibsonjr/">
         <p>
           <strong>LinkedIn</strong>
         </p>
       </a>
-      <a href="mailto:james@gibsunas.co?subject=jamesgibson.co">
+      <a href="mailto:james.the.nodester@gmail.com?subject=jamesgibson.co">
         <p>
-          <strong>Email</strong>
+          <strong>Gmail</strong>
+        </p>
+      </a>
+      <a href="https://github.com/james-gibson">
+        <p>
+          <strong>GitHub</strong>
         </p>
       </a>
     </div>
