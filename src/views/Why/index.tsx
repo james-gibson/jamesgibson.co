a8729066d10552cdd0f131c0586087806c5337b9
James Gibson <james.the.nodester@gmail.com>
2025-09-05 00:24:09 -0600

    add basic why placeholder for dynamic content to start filling

---
+++ src/views/Why/index.tsx
@@ -0,0 +1,14 @@
+
+import headshot from "../../assets/headshot.jpeg";
+
+function Why() {
+  return (
+    <div className="why">
+      <section className="row">
+        <EnhancedQuote/>
+      </section>
+    </div>
+  );
+}
+
+export default Why;
