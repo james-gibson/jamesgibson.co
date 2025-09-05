8ce257f6647c6b4a395effe8a60a0e2832475a96
James Gibson <james.the.nodester@gmail.com>
2025-09-05 00:35:44 -0600

    update to use test gist

--- src/views/Why/index.tsx
+++ src/views/Why/index.tsx
@@ -3,7 +3,7 @@ function Why() {
   return (
     <div className="why">
       <section className="row">
-        <EnhancedQuote/>
+        <EnhancedQuote gistUrl='https://gist.github.com/james-gibson/20f8bbb5801907b605b62fb1344b6686'/>
       </section>
     </div>
   );
